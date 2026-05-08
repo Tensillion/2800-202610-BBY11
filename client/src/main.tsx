903a8bbff823920667b5a7898e2a54decc97382f
Umanga <umangabajgai@gmail.com>
2026-05-07 18:20:45 -0700

    Resolve merge conflict in main.tsx.

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
@@ -22,7 +21,7 @@ createRoot(document.getElementById("root")!).render(
       <Footer />
       <Routes>
         <Route path="/" element={<LandingPage />} />
-        <Route path="/signin" element={<SignUpPage />} />
+        <Route path="/signup" element={<SignUpPage />} />
         <Route path="/pet" element={<PetPage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/profile" element={<ProfilePage />} />
@@ -33,50 +32,4 @@ createRoot(document.getElementById("root")!).render(
       </Routes>
     </BrowserRouter>
   </StrictMode>,
-=======
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import { Routes, Route, BrowserRouter } from 'react-router-dom';
-
-import LandingPage from './LandingPage/LandingPage';
-import SigninPage from './SigninPage/SigninPage';
-import LoginPage from './LoginPage/LoginPage';
-import MapPage from './MapPage/MapPage';
-
-import CameraPage from './CameraPage/CameraPage';
-import CameraResultPage from './CameraPage/ResultPage/CameraResultPage';
-
-import PetPage from './PetPage/PetPage';
-import CollectionPage from './CollectionPage/CollectionPage';
-import ProfilePage from './ProfilePage/ProfilePage';
-import SettingsPage from './SettingsPage/SettingsPage';
-
-import './css/index.css';
-
-createRoot(document.getElementById('root')!).render(
-	<StrictMode>
-		<BrowserRouter>
-			<Routes>
-				<Route path="/" element={<LandingPage />} />
-
-				{/* Authentication Routes */}
-				<Route path="/signin" element={<SigninPage />} />
-				<Route path="/login" element={<LoginPage />} />
-
-				{/* Main App Routes */}
-				<Route path="/map" element={<MapPage />} />
-				<Route path="/camera">
-					<Route index element={<CameraPage />} />
-					<Route path="result" element={<CameraResultPage />} />
-				</Route>
-				<Route path="/pet" element={<PetPage />} />
-				<Route path="/collection" element={<CollectionPage />} />
-				<Route path="/profile" element={<ProfilePage />} />
-
-				{/* Settings Route */}
-				<Route path="/settings" element={<SettingsPage />} />
-			</Routes>
-		</BrowserRouter>
-	</StrictMode>
->>>>>>> 4734e9578a60c15e40b2c7b09d96764fc54ac709
 );
