93a8cd9611d46f0ed4970485b4fc890105823ff9
Tensillion <nguyentyson03@gmail.com>
2026-05-09 03:45:17 -0700

    Implement JSON parsing for results in the camera page, Add CSS for authentication pages, Implemented simple JSON as temp DB for plants edibility
    Added a simple JSON path to the server side. I also finished the Results page for the camera so it now shows the results of the plant to ensure that it's edible based on our temp DB which is the JSON I've stored. CSS for both the login and signup pages, they share auth.css so its consistent on both pages.

--- client/src/SignUpPage/SignUpPage.tsx
+++ client/src/SignUpPage/SignUpPage.tsx
@@ -1,22 +1,35 @@
-import { useNavigate } from 'react-router-dom';
-import type { SyntheticEvent } from 'react';
+import { Link, useNavigate } from "react-router-dom";
+import type { SyntheticEvent } from "react";
+import "../css/auth.css";
 
 function SignUpPage() {
 	const navigate = useNavigate();
 	function handleSubmit(e: SyntheticEvent<HTMLFormElement>) {
 		e.preventDefault();
-		navigate('/pet');
+		navigate("/pet");
 	}
 
 	return (
 		<>
-			<h1>Sign Up</h1>
-			<form onSubmit={handleSubmit}>
-				<input type="text" name="username" placeholder="Username" />
-				<input type="email" name="email" placeholder="Email" />
-				<input type="password" name="password" placeholder="Password" />
-				<input type="submit" value="Submit" />
-			</form>
+			<div className="authentication-header">
+				<img className="logo" src="../public/Logo.png" alt="Logo" />
+				<Link className="nav-link" to="/login">
+					<button className="other-page-btn">Log in</button>
+				</Link>
+			</div>
+			<div className="authentication-container">
+				<h1>Sign Up</h1>
+				<form className="auth-form" onSubmit={handleSubmit}>
+					<input className="auth-input" type="text" name="username" placeholder="Username" />
+					<input className="auth-input" type="email" name="email" placeholder="Email" />
+					<input className="auth-input" type="password" name="password" placeholder="Password" />
+					<input className="auth-submit-btn" type="submit" value="Submit" />
+				</form>
+				<p className="sub-text">
+					By signing up, you agree to our <a href="/terms">Terms</a> and{" "}
+					<a href="/privacy">Privacy Policy</a>.
+				</p>
+			</div>
 		</>
 	);
 }
